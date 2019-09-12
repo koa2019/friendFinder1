34eaf512f20fa065d4077768538ef009bd0b335e
koa2019 <no-reply>
2019-09-12 12:59:29 -0700

    imported & configured an instance of expressjs

---
+++ server.js
@@ -0,0 +1,26 @@
+// dependencies
+var express = require('express');
+
+// config express to create an instance of it
+// creating an express server & saves it to variable for easier reference
+var app = express();
+
+// set PORT for app to use
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing from web browser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// imports html routing calls
+var htmlRoutes = require('./app/routing/htmlRoutes')(app);
+
+// import api get routes
+var apiRoutes = require('./app/routing/apiRoutes')(app);
+
+// tell server to start listening for a requests & console PORT #
+app.listen(PORT, function() {
+
+    console.log("App connected to localhost:" + PORT);
+});
