9ee260aeb67e81f2d2a479ea03e042b7c83751ab
koa2019 <no-reply>
2019-09-12 14:03:30 -0700

    added basic template for home & survey html pages

---
+++ app/data/friends.js
@@ -0,0 +1,149 @@
+// data array of objects
+var friendsData = [{
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Tiffany",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Jacob Deming",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Louis T. Delia",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Lou Ritter",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Jordan Biason",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Seren",
+        "photo": "https://avatars1.githubusercontent.com/u/50847753?s=460&v=4",
+        "scores": [
+            "4",
+            "2",
+            "4",
+            "1",
+            "3",
+            "1",
+            "1",
+            "1",
+            "4",
+            "3"
+        ]
+    },
+    {
+        "name": "Daniel",
+        "photo": "https://www.gannett-cdn.com/media/2019/08/05/USATODAY/usatsports/most-popular-dog-breeds-intro-image.jpg?width=2560",
+        "scores": [
+            "2",
+            "3",
+            "3",
+            "4",
+            "3",
+            "2",
+            "3",
+            "3",
+            "3",
+            "4"
+        ]
+    }
+];
+
+//export friends array
+module.exports = friendsData;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,14 @@
+// link data from a file
+var friends = require('../data/friends');
+
+//export this function to server.js
+module.exports = function(app) {
+
+    // api get calls for api routes
+    //when link is clicked it shows JSON data in friends
+    app.get('/api/friends', function(req, res) {
+
+        return res.json(friends);
+    });
+
+};
