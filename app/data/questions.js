96aae21f45a6fb8ba9436d17499b648358f541ef
koa2019 <no-reply>
2019-09-14 15:31:03 -0700

    render questions select html tags dynamically & capture selected option's value & push to array

---
+++ app/data/questions.js
@@ -0,0 +1,14 @@
+var questions = [
+    'Would you consider yourself a morning person?',
+    'Do you prefer the beach over the mountains?',
+    'Do you ',
+    'Do you ',
+    'Do you ',
+    'Do you ',
+    'Do you ',
+    'Do you ',
+    'Do you ',
+    'Do you '
+];
+
+module.exports = questions;
