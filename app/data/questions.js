e7ce393481e3c80840bed5fad1a2324c7f4388aa
koa2019 <no-reply>
2019-09-17 15:12:10 -0700

    updated gitignore & questions.js data array

--- app/data/questions.js
+++ app/data/questions.js
@@ -1,14 +1,14 @@
 var questions = [
     'Would you consider yourself a morning person?',
     'Do you prefer the beach over the mountains?',
-    'Do you ',
-    'Do you ',
-    'Do you ',
-    'Do you ',
-    'Do you ',
-    'Do you ',
-    'Do you ',
-    'Do you '
+    'Dogs are better than cats',
+    'Dinner & a movie is better than going to a night club',
+    'Sunsets are better than sunrises',
+    'Money can buy you happiness',
+    'Intimacy is the same as sex',
+    'Parents should be able to spank their kids',
+    'Family is important to me',
+    'UFOs are real'
 ];
 
 module.exports = questions;
